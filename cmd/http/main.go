f1df0aa6a21363f678a75d31921436448c522680
itsLeonB <ellionblessan@gmail.com>
2026-02-27 21:04:27 +0700

    fix: adjust main code exit

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/itsLeonB/cashback/internal/adapters/http"
 	"github.com/itsLeonB/cashback/internal/core/config"
@@ -11,16 +12,25 @@ import (
 )
 
 func main() {
+	var exitCode int
+	defer func() {
+		os.Exit(exitCode)
+	}()
+
 	logger.Init("Cashback")
 
 	if err := config.Load(); err != nil {
-		logger.Fatal(err)
+		logger.Error(err)
+		exitCode = 1
+		return
 	}
 
 	ctx := context.Background()
 	otelShutdown, err := otel.InitSDK(ctx, config.Global.OTel)
 	if err != nil {
-		logger.Fatal(err)
+		logger.Error(err)
+		exitCode = 1
+		return
 	}
 	defer func() {
 		if err := otelShutdown(ctx); err != nil {
@@ -30,12 +40,14 @@ func main() {
 
 	srv, shutdownFunc, err := http.Setup(*config.Global)
 	if err != nil {
-		logger.Fatal(err)
+		logger.Error(err)
+		exitCode = 1
+		return
 	}
-
 	defer shutdownFunc()
 
 	if err := srv.ListenAndServe(ctx); err != nil {
 		logger.Error(err)
+		exitCode = 1
 	}
 }
